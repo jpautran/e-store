04d3cb6a2c68316dce58ece141c5a1217ef406bf
Joao Paulo <jpautran1414@gmail.com>
2020-07-15 13:51:03 -0300

    style

--- public/deleteBox.js
+++
@@ -1,8 +0,0 @@
-const formDelete = document.querySelector("#form-delete");
-
-formDelete.addEventListener("submit", function(event) {
-    const confirmation = confirm("Deseja realmente deletea?");
-    if(!confirmation) {
-        event.preventDefault();
-    }
-});
