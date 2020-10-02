00173fb3c00e4bc13d002b5dc28f2c8a88828b78
Joao Paulo <jpautran1414@gmail.com>
2020-10-02 15:22:19 -0300

    restart project

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const routes = express.Router();
-const ProductController = require('./app/controllers/ProductController');
 
 routes.get('/', function(req, res) {
-    return res.render("layout.njk");
+    return res.send("ok");
 });
-
-routes.get('/products/create', ProductController.create);
-routes.post('/products', ProductController.post);
-
-// Alias
-routes.get('/ads/create', function(req, res) {
-    return res.redirect("/products/create");
-});
-
 
 module.exports = routes;
